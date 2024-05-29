97b7b5917deb28579128d1319075771cd6678148
zsikorska <sikorska.zuzia@gmail.com>
2024-05-29 23:19:03 +0200

    get username

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react";
 import Cookies from 'js-cookie';
 import {useNavigate} from "react-router-dom";
+import getUsernameFromToken from "./util/GetUsernameFromToken";
 
 const Home = () => {
     const cognitoUrl = process.env.REACT_APP_COGNITO_URL;
@@ -32,16 +33,19 @@ const Home = () => {
                 body: body
             })
                 .then(response => response.json())
-                .then(data => {
+                .then(async data => {
                     console.log(data);
                     const access_token = data.access_token;
                     const id_token = data.id_token;
                     const refresh_token = data.refresh_token;
 
                     const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
-                    Cookies.set('COGNITO_TOKEN', access_token, { secure: true, expires: expirationDate });
-                    Cookies.set('COGNITO_ID_TOKEN', id_token, { secure: true, expires: expirationDate });
-                    Cookies.set('COGNITO_REFRESH_TOKEN', refresh_token, { secure: true});
+                    Cookies.set('COGNITO_TOKEN', access_token, {secure: true, expires: expirationDate});
+                    Cookies.set('COGNITO_ID_TOKEN', id_token, {secure: true, expires: expirationDate});
+                    Cookies.set('COGNITO_REFRESH_TOKEN', refresh_token, {secure: true});
+
+                    const username = await getUsernameFromToken(id_token);
+                    Cookies.set('USERNAME', username, {secure: true});
 
                     navigate("/dashboard");
                 })
--- client/src/util/Logout.js
+++ client/src/util/Logout.js
@@ -4,6 +4,7 @@ function handleSignOut (navigate) {
     Cookies.remove('COGNITO_TOKEN');
     Cookies.remove('COGNITO_ID_TOKEN');
     Cookies.remove('COGNITO_REFRESH_TOKEN');
+    Cookies.remove('USERNAME');
     navigate("/");
 }
 
